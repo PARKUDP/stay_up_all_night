7222690483d3af8749733a3258ab8b2cc3ab9750
park <park@ieusernoMacBook-Air.local>
2024-10-01 16:14:43 +0900

    ログイン画面フォマート追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Classes from './components/Classes';
 import Assignments from './components/Assignments';
+import Login from './components/login';
 
 function App() {
+    const handleLogin = (email: string, password: string) => {
+      console.log('ログインされたメールアドレス:', email, 'とパスワード:', password);
+    };
+
   return (
     <Router>
       <Routes>
+        <Route path='/login' element={<Login onSubmit={handleLogin} />} />
         <Route path="/" element={<Classes />} />
         <Route path="/assignments/:classId" element={<Assignments />} />
       </Routes>
     </Router>
-  );
+    );
 }
+
 
 export default App;
